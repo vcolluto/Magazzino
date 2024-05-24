9e53a85eebbd4b0b9bc5c635aeefb8822de84d87
Vincenzo Colluto <enzocoll@tiscali.it>
2024-05-24 09:50:38 +0200

    menu di scelta

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,42 +1,109 @@
 package org.generation.italy;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		HashMap<String, String> elencoFornitori= new HashMap<String, String>(){{		
-			put("F01","Mario Rossi");
-			put("F02","Franco Verdi");
-			put("F03","Gino Gialli");			
-		}};
-		
-		HashMap<String, String> elencoClienti= new HashMap<String, String>(){{		
-			put("C01","Franca Rame");
-			put("C02","Pina Acciaio");
-			put("C03","Lina Bronzo");			
-		}};
-		
-		HashMap<String, String> elencoProdotti= new HashMap<String, String>(){{		
-			put("P01","Pane");
-			put("P02","Pasta");
-			put("P03","Biscotti al cioccolato");			
-		}};
-		
-		HashMap<String, String> tipologieMovimento= new HashMap<String, String>(){{		
-			//movimenti in entrata
-			put("E01","acquisto da fornitore");
-			put("E02","reso da cliente");
-			put("E03","produzione interna");			
-			put("E04","spostamento da altro magazzino");
-			
-			//movimenti in uscita
-			put("U01","vendita a cliente");
-			put("U02","reso a fornitore");
-			put("U03","sostituzione in garanzia");			
-			put("U04","spostamento a altro magazzino");
-		}};
+		HashMap<String, String> elencoFornitori = new HashMap<String, String>() {
+			{
+				put("F01", "Mario Rossi");
+				put("F02", "Franco Verdi");
+				put("F03", "Gino Gialli");
+			}
+		};
 
+		HashMap<String, String> elencoClienti = new HashMap<String, String>() {
+			{
+				put("C01", "Franca Rame");
+				put("C02", "Pina Acciaio");
+				put("C03", "Lina Bronzo");
+			}
+		};
+
+		HashMap<String, String> elencoProdotti = new HashMap<String, String>() {
+			{
+				put("P01", "Pane");
+				put("P02", "Pasta");
+				put("P03", "Biscotti al cioccolato");
+			}
+		};
+
+		HashMap<String, String> tipologieMovimento = new HashMap<String, String>() {
+			{
+				// movimenti in entrata
+				put("E01", "acquisto da fornitore");
+				put("E02", "reso da cliente");
+				put("E03", "produzione interna");
+				put("E04", "spostamento da altro magazzino");
+
+				// movimenti in uscita
+				put("U01", "vendita a cliente");
+				put("U02", "reso a fornitore");
+				put("U03", "sostituzione in garanzia");
+				put("U04", "spostamento a altro magazzino");
+			}
+		};
+
+		Scanner sc = new Scanner(System.in);
+		String scelta;
+		do {
+			System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n**** GESTIONE MAGAZZINO *****\n");
+			System.out.println("1. Inserimento movimento in entrata");
+			System.out.println("2. Inserimento movimento in uscita");
+			System.out.println("3. Visualizzazione movimento in entrata");
+			System.out.println("4. Visualizzazione movimento in uscita");
+			System.out.println("5. Calcolo giacenza prodotto\n");
+			System.out.println("6. Esci");
+
+			System.out.print("\n\nInserisci la tua scelta: ");
+			scelta = sc.nextLine();
+
+			/*
+			 * PRIMA possibilità: if/else if
+			 * 
+			 * if (scelta.equals("1")) {
+			 * 
+			 * } else if (scelta.equals("2")) {
+			 * 
+			 * } else if (scelta.equals("3")) {
+			 * 
+			 * } // ecc. ecc.
+			 */
+
+			switch (scelta) {
+			case "1":
+				// codice per il caso "1"
+				System.out.println("Inserimento movimento in entrata");
+				break;
+			case "2":
+				// codice per il caso "2"
+				System.out.println("Inserimento movimento in uscita");
+				break;
+			case "3":
+				System.out.println("Visualizzazione movimenti in entrata");
+				// codice per il caso "3"
+				break;
+			case "4":
+				// codice per il caso "4"
+				System.out.println("Visualizzazione movimenti in uscita");
+				break;
+			case "5":
+				// codice per il caso "5"
+				System.out.println("Calcolo giacenza");
+				break;
+			case "6":
+				// codice per il caso "6"
+				System.out.println("Arrivederci!");
+				break;
+			default: // tutti gli altri casi verranno gestiti qui
+				// codice per gestire tutti gli altri casi
+				System.out.println("Scelta non valida!");
+			}
+			System.out.println("Premi invio per continuare...");
+			sc.nextLine();
+		} while (!scelta.equals("6")); // torno indietro se la scelta è diversa da 6
 	}
 
 }
